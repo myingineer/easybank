8d2e498ea6e17b098878cd1242a7f6e3629b51bd
myingineer <e.alexander190@yahoo.com>
2023-08-31 16:39:39 +0100

    version-2.0

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ mobileMenu.addEventListener('click', () => {
         dropDown.style.display = 'block';
         mobileMenu.src = './images/icon-close.svg';
         document.querySelector('.first-container').style.opacity = '60%';
+        document.body.addEventListener('click', (e) => {
+            if (!mobileMenu.contains(e.target)) {
+                dropDown.style.display = '';
+                document.querySelector('.first-container').style.opacity = '';
+                mobileMenu.src = './images/icon-hamburger.svg';
+            };
+        });
     } else {
         mobileMenu.src = './images/icon-hamburger.svg';
         dropDown.style.display = '';
